f2bfaa2956e954af8b6a61eb68b453504e34dcb0
Rafael <rafaelximenesaraujo@outlook.com>
2024-03-22 02:46:04 -0300

    primeiro commit e entrada de path com widgets tipo Text

--- Conversões/main.py
+++ Conversões/main.py
@@ -1,9 +1,8 @@
-import pathlib
 from tkinter import *
 import tkinter.filedialog
 import win32com
+from win32com import client
 root = Tk()
-
 
 class Application:
 
@@ -24,47 +23,51 @@ class Application:
 
     def frame_da_tela(self):
         self.frame_1 = Frame(self.root, bd=4, bg='#ddd9ce', highlightbackground='black', highlightthickness=3)
-        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
+        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.56)
 
         self.frame_2= Frame(self.root, bd=4, bg='#ddd9ce', highlightbackground='black', highlightthickness=3)
-        self.frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
+        self.frame_2.place(relx=0.02, rely=0.6, relwidth=0.96, relheight=0.36)
 
     def widgets_frame1(self):
-        self.frame_1 = tkinter.Frame(self.root, bd=4, bg='#ddd9ce', highlightbackground='black', highlightthickness=3)
-        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
-
-        # botão de busca 1
+        # botões de busca
         self.bt_buscar_1 = tkinter.Button(self.frame_1, text='buscar', command=self.buscar_1)
-        self.bt_buscar_1.place(relx=0.89, rely=0.09, relwidth=0.1, relheight=0.15)
+        self.bt_buscar_1.place(relx=0.01, rely=0.24, relwidth=0.1, relheight=0.1)
 
-        # botão de buscar 2
         self.bt_buscar_2 = tkinter.Button(self.frame_1, text='buscar', command=self.buscar_2)
-        self.bt_buscar_2.place(relx=0.89, rely=0.37, relwidth=0.1, relheight=0.15)
+        self.bt_buscar_2.place(relx=0.01, rely=0.61, relwidth=0.1, relheight=0.1)
+        # ddddce
 
-        # label e text do caminho
-        self.lb_path = Label(self.frame_1, text='caminho do arquivo original', background='#ddddce')
-        self.lb_path.place(relx=0.01, rely=0.001, relwidth=0.22, relheight=0.1)
+        # Entry
+        self.Entry_1 = Entry(self.frame_1)
+        self.Entry_1.place(relx=0.01, rely=0.115, relwidth=0.98, relheight=0.09)
+
+        self.Entry_2 = Entry(self.frame_1)
+        self.Entry_2.place(relx=0.01, rely=0.49, relwidth=0.98, relheight=0.09)
+
+        self.Entry_name = Entry(self.frame_1)
+        self.Entry_name.place(relx=0.01, rely=0.85, relwidth=0.98, relheight=0.09)
+
+        # label e text de busca do caminho
+        self.lb_path = Label(self.frame_1, text='Caminho do arquivo original', background='#dddddd')
+        self.lb_path.place(relx=0.01, rely=0.005, relwidth=0.22, relheight=0.08)
         self.frame_text = Frame(self.frame_1)
-        self.frame_text.place(relx=0.01, rely=0.1, relwidth=0.87, relheight=0.12)
-        self.path_text = Text(self.frame_text)
+        self.frame_text.place(relx=0.115, rely=0.25, relwidth=0.86, relheight=0.08)
+        self.path_text = Text(self.frame_text, background='#dddddd')
+        self.path_text.configure(state='disabled')
         self.path_text.pack(expand=True)
 
-        # label e text do novo caminho
-        self.lb_new_path = Label(self.frame_1, text='local do novo arquivo', background='#ddddce')
-        self.lb_new_path.place(relx=0.01, rely=0.24, relwidth=0.17, relheight=0.14)
+        #label do nome
+        self.lb_name = Label(self.frame_1, text='Nome do arquivo', background='#dddddd')
+        self.lb_name.place(relx=0.01, rely=0.74, relwidth=0.14, relheight=0.09)
+
+        # label e text de busca do novo caminho
+        self.lb_new_path = Label(self.frame_1, text='Local do novo arquivo', background='#dddddd')
+        self.lb_new_path.place(relx=0.01, rely=0.38, relwidth=0.17, relheight=0.08)
         self.frame_new_path = Frame(self.frame_1)
-        self.frame_new_path.place(relx=0.01, rely=0.39, relwidth=0.87, relheight=0.12)
-        self.new_path_text = Text(self.frame_new_path)
+        self.frame_new_path.place(relx=0.115, rely=0.62, relwidth=0.87, relheight=0.08)
+        self.new_path_text = Text(self.frame_new_path, background='#dddddd')
         self.new_path_text.configure(state='disabled')
         self.new_path_text.pack(expand=True, fill='x')
-
-
-        # label e entrada do nome do aquivo convertido
-        self.lb_name = Label(self.frame_1, text='nome do arquivo', background='#ddddce')
-        self.lb_name.place(relx=0.01, rely=0.55, relwidth=0.14, relheight=0.08)
-
-        self.new_name = Entry(self.frame_1)
-        self.new_name.place(relx=0.01, rely=0.65, relwidth=0.87, relheight=0.1)
 
     def widgets_frame2(self):
         # criando botão de word2pdf
@@ -76,20 +79,24 @@ class Application:
 
 # criando função de conversão
     def buscar_1(self):
+        self.path_text.configure(state='normal')
         self.path_orig = tkinter.filedialog.askopenfile(mode='r', initialdir='/Documentos',
                                                         initialfile='C:/Users/rafae/OneDrive/Área de Trabalho'
                                                                     '/Memorial_descritivo.docx',
                                                         title="Selecione um arquivo",
                                                         filetypes=(
                                                             ("Arquivos doc", ".docx"), ("arquivos xlsx", ".xlsx")))
+        a = self.path_orig.name.replace('/', '\\')
         self.path_text.delete(1.0, 'end')
-        self.path_text.insert(1.0, self.path_orig.name)
+        self.path_text.insert(1.0, a)
+        self.path_text.configure(state='disabled')
 
     def buscar_2(self):
         self.new_path_text.configure(state='normal')
         self.new_path = tkinter.filedialog.askdirectory(initialdir='/Documentos', title="Selecione um arquivo")
+        a = self.new_path.replace('/', '\\')
         self.new_path_text.delete(1.0, 'end')
-        self.new_path_text.insert(1.0, self.new_path)
+        self.new_path_text.insert(1.0, a)
         self.new_path_text.configure(state='disabled')
 
     def excel_for_pdf(self):
@@ -107,18 +114,15 @@ class Application:
     def word_for_pdf(self):
         wdFormatPDF = 17
 
-        self.entrada = self.path_text.get(1.0, 'end')
-        self.saida = self.new_path_text.get(1.0, 'end')
+        self.entrada = self.Entry_1.get()
+        self.saida = self.Entry_2.get()
 
-        self.x = pathlib.PurePath(self.entrada)
-        self.y = self.saida
-
-        print(self.x)
-        print(type(self.x))
+        print(self.entrada)
+        print('ag')
 
         word = win32com.client.Dispatch('Word.Application')
-        doc = word.Documents.Open(self.x)
-        doc.SaveAs(self.y, FileFormat=wdFormatPDF)
+        doc = word.Documents.Open(self.entrada)
+        doc.SaveAs(self.saida, FileFormat=wdFormatPDF)
         doc.Close()
         word.Quit()
 
